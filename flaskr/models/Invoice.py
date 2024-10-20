1d54eb99a7104de7fef53cf5acd9f80e00e7ecbd
migueltovar <loganitsolutions@outlook.com>
2024-10-20 00:19:51 -0500

    adding endpoints payment

--- flaskr/models/Invoice.py
+++ flaskr/models/Invoice.py
@@ -9,50 +9,44 @@ class Invoice:
         id (uniqueidentifier): id invoice
         customer_id (UUID): id customer invoice
         invoice_id (str): invoice number
-        payment_id (Optional[UUID]): id payment
+        plan_id (Optional[UUID]): id plan
         amount (float): amount invoice
         tax (float): value of taxes
         total_amount (float): total amount
-        subscription (str): suscription name
-        subscription_id (UUID): suscription id
         status (str): invoice status
         created_at (datetime): creation date
-        updated_at (datetime): update date
+        start_at (datetime): start date
         generation_date (datetime): generation date
-        period (datetime): period invoice
+        end_at (datetime): end date invoice
     """
-    def __init__(self, id: UUID, customer_id: UUID, invoice_id: str, payment_id: Optional[UUID], amount: float, tax: float,
-                 total_amount: float, subscription: str, subscription_id: UUID, status: str,
-                 created_at: datetime, updated_at: datetime, generation_date: datetime, period: datetime):
+    def __init__(self, id: UUID, customer_id: UUID, invoice_id: str, plan_id: Optional[UUID], amount: float, tax: float,
+                 total_amount: float, status: str,
+                 created_at: datetime, start_at: datetime, generation_date: datetime, end_at: datetime):
         self.id = id
         self.customer_id = customer_id
         self.invoice_id = invoice_id
-        self.payment_id = payment_id
+        self.plan_id = plan_id
         self.amount = amount
         self.tax = tax
         self.total_amount = total_amount
-        self.subscription = subscription
-        self.subscription_id = subscription_id
         self.status = status
         self.created_at = created_at
-        self.updated_at = updated_at
+        self.start_at = start_at
         self.generation_date = generation_date
-        self.period = period
+        self.end_at = end_at
 
     def to_dict(self):
         return {
             'id': str(self.id),
             'customerId': str(self.customer_id),
             'invoiceId': str(self.invoice_id),
-            'paymentId': str(self.payment_id) if self.payment_id else None,
+            'plan_id': str(self.plan_id) if self.plan_id else None,
             'amount': self.amount,
             'tax': self.tax,
             'totalAmount': self.total_amount,
-            'subscription': self.subscription,
-            'subscriptionId': str(self.subscription_id),
             'status': self.status,
             'createdAt': self.created_at if self.created_at else None,
-            'updatedAt': self.updated_at if self.updated_at else None,
+            'start_at': self.start_at if self.start_at else None,
             'generationDate': self.generation_date if self.generation_date else None,
-            'period': self.period if self.period else None
+            'end_at': self.end_at if self.end_at else None
         }
