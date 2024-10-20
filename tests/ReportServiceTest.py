9d0821dd5b8ab2a61bbb3488aad08832acf1c3a9
migueltovar <loganitsolutions@outlook.com>
2024-10-20 12:42:28 -0500

    fixing unit test

--- tests/ReportServiceTest.py
+++ tests/ReportServiceTest.py
@@ -2,13 +2,12 @@ import unittest
 from unittest.mock import patch, MagicMock
 from flaskr.service.ReportService import ReportService
 from config import Config 
-
 class ReportServiceTestCase(unittest.TestCase):
 
     def setUp(self):
         self.service = ReportService()
 
-    @patch('flaskr.service.ReportService.requests.get') 
+    @patch('flaskr.service.requests.get') 
     def test_download_invoice_by_id_success(self, mock_get):
 
         mock_response = MagicMock()
@@ -22,7 +21,7 @@ class ReportServiceTestCase(unittest.TestCase):
         self.assertIsNotNone(pdf_document)
         self.assertEqual(pdf_document, b'PDF document content')
 
-    @patch('flaskr.service.ReportService.requests.get')
+    @patch('flaskr.service.requests.get')
     def test_download_invoice_by_id_not_found(self, mock_get):
 
         mock_response = MagicMock()
@@ -34,7 +33,7 @@ class ReportServiceTestCase(unittest.TestCase):
 
         self.assertIsNone(pdf_document)
 
-    @patch('flaskr.service.ReportService.requests.get')
+    @patch('flaskr.service.requests.get')
     def test_download_invoice_by_id_error(self, mock_get):
   
         mock_get.side_effect = Exception("Connection Error")
