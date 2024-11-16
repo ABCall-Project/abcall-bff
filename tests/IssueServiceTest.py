987b0c6f8206e3425f88a887c62c56558da9c426
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-11-16 16:05:12 -0500

    Update IssueServiceTest.py

--- tests/IssueServiceTest.py
+++ tests/IssueServiceTest.py
@@ -60,6 +60,24 @@ def test_return_a_list_of_issues(self, get_mock):
     response = issueService.get_all_issues()
 
     self.assertEqual(response, issues)
+
+@patch('requests.post')
+def test_should_be_assign_an_issue(self, get_mock):
+        return_message = "Some error ocurred trying to assign_issue issues"
+        issue =  IssueBuilder().build()
+        issue.id
+        fake = Faker()
+        auth_user_agent_id = str(fake.uuid4())
+        data = {
+            "auth_user_agent_id": auth_user_agent_id
+        }
+
+        response = self.client.post(
+        f'/issues/assignIssue?user_id={issue.id}',
+        json=data  
+    )
+
+        
     
 @patch('requests.post')
 def test_should_return_internal_server_error_if_some_error_occurred(self, get_mock):
